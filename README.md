9bf38360cfdce86233e0390d7d7c20699260857f
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-11-20 00:38:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+![npm downloads total](https://img.shields.io/npm/dt/solidjs-material-spinner.svg) ![npm version](https://img.shields.io/npm/v/solidjs-material-spinner.svg) ![npm license](https://img.shields.io/npm/l/solidjs-material-spinner.svg)
+
+# solidjs-material-spinner
+A simple solid-js spinner following Material UI's using only css.
+
+![Example of Spinner](https://raw.githubusercontent.com/icarus-sullivan/solidjs-material-spinner/master/example.gif)
+
+### Installation
+```
+npm install --save solidjs-material-spinner
+```
+or
+```
+yarn add solidjs-material-spinner
+```
+
+### Props
+|Name|Type|Default|Description|
+|-----|-----|-----|-----|
+|radius |Number |40 |The radius of the spinner |
+|color |String |#333333 |The color of the spinner |
+|stroke |Number |5 |The spinner's stroke width |
+|visible|Boolean |true |Whether to show the spinner or not|
+
+### Usage
+Example:
+```
+import Spinner from 'solidjs-material-spinner';
+
+export default () => (
+  <div>
+    <Spinner radius={40} color="#333" stroke={5} />
+  </div>
+)
+```
+
+### Changelog
+
+**1.0.0**
+- Initial publication
---
+++ src/index.tsx
@@ -0,0 +1,112 @@
+import { createEffect, mergeProps, Show } from 'solid-js';
+
+interface Props {
+  radius?: number | string,
+  color?: string,
+  stroke?: number | string,
+  visible?: boolean,
+  [key: string]: any
+}
+
+const DEFAULT_PROPS = {
+  color: '#333',
+  radius: 80,
+  stroke: 10,
+}
+
+const css = `
+.react-spinner-material {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  border: 10px solid #333;
+  box-sizing: border-box;
+  -webkit-animation: rsm-sweep 1s linear alternate infinite, rsm-rotate 0.8s linear infinite;
+          animation: rsm-sweep 1s linear alternate infinite, rsm-rotate 0.8s linear infinite;
+}
+@keyframes rsm-rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes rsm-rotate {
+  from {
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes rsm-sweep {
+  0% {
+    -webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
+    clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
+  }
+  50% {
+    -webkit-clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
+    clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
+  }
+  100% {
+    -webkit-clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
+    clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
+  }
+}
+@-webkit-keyframes rsm-sweep {
+  0% {
+    -webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
+  }
+  50% {
+    -webkit-clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
+  }
+  100% {
+    -webkit-clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
+  }
+}
+`;
+
+const ID = 'spinner_id_style';
+
+export default (props: Props) => {
+  const { visible = true, color, stroke, radius } = mergeProps(DEFAULT_PROPS, props);
+  if (!visible) {
+    return null;
+  }
+
+  createEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+  
+    if (!document.getElementById(ID)) {
+      const head = document.head || document.getElementsByTagName('head')[0];
+      const sprc: any = document.createElement('style');
+      sprc.id = ID;
+      sprc.type = "text/css";
+      if( sprc.styleSheet ) {
+        sprc.styleSheet.cssText = css;
+      } else {
+        sprc.appendChild(document.createTextNode(css));
+      }
+      if( head ) {
+        head.appendChild(sprc);
+      }
+    }
+  })
+
+  return (
+    <Show when={visible}>
+      <div
+          class="react-spinner-material"
+          style={{
+            width: `${radius}px`,
+            height: `${radius}px`,
+            'border-color': color,
+            'border-width': `${stroke}px`,
+          }} 
+        />
+    </Show>
+  );
+}
