5f21af8d57b63564efc24b61b0c2a7a46fb13f3c
Chris Sullivan <chrissullivan.dev@gmail.com>
2022-01-02 23:49:31 -0800

    Adding babel sass plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createEffect, mergeProps, Show } from 'solid-js';
 
-type VisibleProp = () => boolean;
+import './style.scss';
 
+type VisibleProp = () => boolean;
 interface Props {
   radius?: number | string,
   color?: string,
@@ -16,61 +17,6 @@ const DEFAULT_PROPS = {
   stroke: 10,
   visible: () => true,
 }
-
-const css = `
-.solidjs-spinner-material {
-  width: 80px;
-  height: 80px;
-  border-radius: 50%;
-  border: 10px solid #333;
-  box-sizing: border-box;
-  -webkit-animation: rsm-sweep 1s linear alternate infinite, rsm-rotate 0.8s linear infinite;
-          animation: rsm-sweep 1s linear alternate infinite, rsm-rotate 0.8s linear infinite;
-}
-@keyframes rsm-rotate {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-@-webkit-keyframes rsm-rotate {
-  from {
-    -webkit-transform: rotate(0deg);
-  }
-  to {
-    -webkit-transform: rotate(360deg);
-  }
-}
-@keyframes rsm-sweep {
-  0% {
-    -webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
-    clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
-  }
-  50% {
-    -webkit-clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
-    clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
-  }
-  100% {
-    -webkit-clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
-    clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
-  }
-}
-@-webkit-keyframes rsm-sweep {
-  0% {
-    -webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 50% 50%, 0% 0%, 0% 0%, 0% 0%);
-  }
-  50% {
-    -webkit-clip-path: polygon(0% 0%, 0% 100%, 0% 100%, 50% 50%, 100% 0%, 100% 0%, 0% 0%);
-  }
-  100% {
-    -webkit-clip-path: polygon(0% 0%, 0% 100%, 100% 100%, 50% 50%, 100% 100%, 100% 0%, 0% 0%);
-  }
-}
-`;
-
-const ID = 'spinner_id_style';
 
 export default (props: Props) => {
   const { visible, color, stroke, radius } = mergeProps(DEFAULT_PROPS, props);
